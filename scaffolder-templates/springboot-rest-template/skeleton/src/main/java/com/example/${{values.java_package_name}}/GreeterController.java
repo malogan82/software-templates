e42b871d6a2f2ca74be4d40cac8d7f822abb81c6
Marco Longobardi <marco.longobardi@accenture.com>
2023-06-04 10:03:18 +0200

    Modifica per package

--- scaffolder-templates/springboot-rest-template/skeleton/src/main/java/com/example/${{values.java_package_name}}/GreeterController.java
+++ scaffolder-templates/springboot-rest-template/skeleton/src/main/java/com/example/${{values.java_package_name}}/GreeterController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-{{values.java_package_name}};
-
 @RestController
 public class GreeterController {
 
