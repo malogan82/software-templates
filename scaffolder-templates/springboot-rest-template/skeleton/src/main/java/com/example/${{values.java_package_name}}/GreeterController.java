6e1e99a4d5b6abe8cd073cd2a2eed8a8471b8376
Marco Longobardi <mrclngbrd@gmail.com>
2023-06-04 09:45:01 +0200

    Modifica aggiunta spring boot rest template

---
+++ scaffolder-templates/springboot-rest-template/skeleton/src/main/java/com/example/${{values.java_package_name}}/GreeterController.java
@@ -0,0 +1,19 @@
+package com.example.${{values.java_package_name}};
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+{{values.java_package_name}};
+
+@RestController
+public class GreeterController {
+
+	@GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String retrieveScenario(@RequestParam String name) {
+		return "Hello "+name+" !";
+	}
+	
+}
