15d32d2be09a0a58807af01ee9c6aae228fd3cdf
Marco Longobardi <mrclngbrd@gmail.com>
2023-06-05 06:29:20 +0200

    Update GreeterController.java

--- scaffolder-templates/springboot-rest-template/skeleton/src/main/java/com/example/${{values.java_package_name}}/GreeterController.java
+++ scaffolder-templates/springboot-rest-template/skeleton/src/main/java/com/example/${{values.java_package_name}}/GreeterController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreeterController {
